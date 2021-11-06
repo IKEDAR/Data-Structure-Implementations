b9fddf21b55d51c0c9487894c1c9a9bb6cef50dc
IKEDAR <34489504+IKEDAR@users.noreply.github.com>
2021-11-07 01:31:19 +0530

    Add files via upload

---
+++ Linked list/RecursivePrintingLinkedList.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+    int data;
+    struct Node* next;
+};
+struct Node* head;
+
+void insert(int x,int n){
+  struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+  temp -> data = x;
+  temp -> next = NULL;
+  if(n ==1){
+    temp -> next= head;
+    head= temp;
+    return;
+  }
+  struct Node* temp1=(struct Node*)malloc(sizeof(struct Node));
+  temp1=head;
+  int i;
+  for(i=0;i<n-2;i++){
+    temp1=temp1 -> next;
+  }
+  temp -> next= temp1 -> next;
+  temp1 -> next=temp;
+  return;
+  
+}
+/*void print(){
+   struct Node* temp=head;
+    /*printf("The list is\n"); 
+    while(temp!=NULL){
+        printf("%d\t",temp -> data);
+        temp=temp -> next;
+}
+} */
+
+void Print(struct Node* p)
+{
+    if (p==NULL){
+        return;
+    }
+    printf("%d\t",p->data);
+    Print(p->next);
+}
+
+
+    
+    
+
+
+int main()
+{
+    head=NULL;
+    insert(2,1); //2
+    insert(3,2); // 2 3
+    insert(4,1); // 4 2 3
+    insert(5,3); // 4 2 5 3 
+    Print(head);
+    
+    
+    return 0;
+    
+};
