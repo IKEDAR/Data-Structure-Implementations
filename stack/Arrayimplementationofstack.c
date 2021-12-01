bd17391f1123fc2903a559dec86c74a30c7b3900
IKEDAR <34489504+IKEDAR@users.noreply.github.com>
2021-12-01 21:48:03 +0530

    Update Arrayimplementationofstack.c

--- stack/Arrayimplementationofstack.c
+++ stack/Arrayimplementationofstack.c
@@ -1,26 +1,27 @@
-#include <stdio.h>
-#define MAXSIZE 101
-int A[MAXSIZE];
+#include <stdio.h> 
+#define MAXSIZE 101 /*Defining macro for Array size to be used*/
+int A[MAXSIZE]; /*Array of MAXSIZE*/
 int top=-1;
 
-void Push(int x)
+void Push(int x) /*Function to push an element on top of stack*/
 {
-    if(top==MAXSIZE-1){
+    if(top==MAXSIZE-1){ /*If stack has reached its maxsize then return Stack Overflow*/
         printf("Stack Overflow\n");
         return;
     }
-    A[++top]=x;
+    A[++top]=x; /*If stack has not reached its maxsize then push element x on array position denoted by ++top*/
 }
 
-void Pop()
+void Pop() /*To remove element which is at top of stack*/
 {
-    if(top==-1){
+    if(top==-1){ /*if top is equal to -1 then there is no element in stack*/
         printf("No element to Pop");
         return;
     }
-    top--;
-}
-int print()
+    top--; /*reduce value of top variable by one*/
+} 
+
+int print() /*Print the stack*/
 {
     int i=0;
     for(i=0;i<=top;i++)
@@ -30,12 +31,12 @@ int print()
     printf("\n");
 }
 
-int Top(){
+int Top(){ /*Return the value which is at top of stack*/
     return printf("%d\n",A[top]);
 }
 
 int main(void) {
-	// your code goes here
+	
 	Push(2);
 	Push(4);
 	Push(5);
